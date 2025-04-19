a169802e2515314ad5a5b898fc0ae2992f69bc77
Hammadniazi <hammadniazi991@gmail.com>
2025-04-19 13:54:58 +0200

    call the api for login page and did validation for email and css

--- src/constant.mjs
+++ src/constant.mjs
@@ -1,3 +1,4 @@
 export const registerApi_url = "https://v2.api.noroff.dev/auth/register";
+export const loginApi_url = "https://v2.api.noroff.dev/auth/login";
 export const blogApi_url = "https://v2.api.noroff.dev/blog/posts/Hammad";
 export const error_message_default = "Something is wrong!";
