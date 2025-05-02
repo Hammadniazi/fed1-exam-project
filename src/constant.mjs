e2ac4feeb121891545e0f0330fce31fd0e7d2b90
Hammadniazi <hammadniazi991@gmail.com>
2025-05-02 15:07:58 +0200

    create post page and through query prameter if call the api

--- src/constant.mjs
+++ src/constant.mjs
@@ -1,4 +1,5 @@
 export const registerApi_url = "https://v2.api.noroff.dev/auth/register";
 export const loginApi_url = "https://v2.api.noroff.dev/auth/login";
 export const blogApi_url = "https://v2.api.noroff.dev/blog/posts/Hammad";
+export const postPage_url = "https://v2.api.noroff.dev/blog/posts/Hammad";
 export const error_message_default = "Something is wrong!";
